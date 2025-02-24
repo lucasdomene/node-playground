be18fac4f44bf3bbac6d7498b8f6ec4786a13cb8
Lucas Domene <lucas.firmo@gmail.com>
2025-02-24 16:07:47 -0300

    get tours endpoint

--- natours/app.js
+++ natours/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const port = 3000;
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello World from GET', app: 'Natours' });
-});
 
-app.post('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World from POST', app: 'Natours' });
-});
